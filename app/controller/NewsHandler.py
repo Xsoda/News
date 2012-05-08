3d083f8dc919171237e9dbe901c021e779e4df4f
Xsoda <Xsoda@live.cn>
2012-05-08 11:02:03 +0800

    fix: doc

--- app/controller/NewsHandler.py
+++ app/controller/NewsHandler.py
@@ -5,7 +5,7 @@ import time
 
 class ShowNews(BaseHandler):
     def get(self, id):
-        news = self.db.get("select news.id, news.title, news.content, news.postedat, news.commentnum, usr.name as author, category.name as category, news.categoryid from news left join usr on news.author=usr.id left join category on news.categoryid=category.id where news.id=%s;" % id)
+        news = self.db.get("select news.id, news.title, news.content, news.postedat, news.commentnum, usr.name as author, category.name as category, doc, news.categoryid from news left join usr on news.author=usr.id left join category on news.categoryid=category.id where news.id=%s;" % id)
         if news:
             news['content'] = self.DocParise(news['content'], news['doc'])
             self.write(self.serve_template("news.html", **{'news':news}))
