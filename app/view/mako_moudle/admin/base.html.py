ef8986bbfff7e7ea72925192b1997127e2228abd
Xsoda <Xsoda@live.cn>
2012-05-10 10:49:59 +0800

    fix favicon.ico and comment default image

--- app/view/mako_moudle/admin/base.html.py
+++ app/view/mako_moudle/admin/base.html.py
@@ -4,7 +4,7 @@ UNDEFINED = runtime.UNDEFINED
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 7
-_modified_time = 1336401900.156
+_modified_time = 1336617757.89
 _template_filename = 'app/view/admin/base.html'
 _template_uri = 'admin/base.html'
 _source_encoding = 'utf-8'
@@ -22,20 +22,20 @@ def render_body(context,**pageargs):
             return render_extra(context.locals_(__M_locals))
         __M_writer = context.writer()
         # SOURCE LINE 1
-        __M_writer('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"\r\n        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\r\n<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">\r\n        <head>\r\n                <meta http-equiv="Content-type" content="text/html; charset=utf-8" />\r\n                <title>PPTM News 管理后台</title>\r\n                <link rel="stylesheet" href="../../static/stylesheets/admin-css/960.css" type="text/css" media="screen" charset="utf-8" />\r\n                <link rel="stylesheet" href="../../static/stylesheets/admin-css/template.css" type="text/css" media="screen" charset="utf-8" />\r\n                <link rel="stylesheet" href="../../static/stylesheets/admin-css/colour.css" type="text/css" media="screen" charset="utf-8" />\r\n                <!--[if IE]><![if gte IE 6]><![endif]-->\r\n                <script src="../../static/javascripts/admin-js/glow/1.7.0/core/core.js" type="text/javascript"></script>\r\n                <script src="../../static/javascripts/jquery-1.7.2.min.js" type="text/javascript"></script>\r\n                <script src="../../static/javascripts/admin-js/glow/1.7.0/widgets/widgets.js" type="text/javascript"></script>\r\n                <script src="../../static/javascripts/displayMessage.js" type="text/javascript"></script>\r\n                <link href="../../static/javascripts/admin-s/glow/1.7.0/widgets/widgets.css" type="text/css" rel="stylesheet" />\r\n                ')
+        __M_writer('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"\r\n        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\r\n<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">\r\n        <head>\r\n                <meta http-equiv="Content-type" content="text/html; charset=utf-8" />\r\n                <title>PPTM News 管理后台</title>\r\n                <link rel="stylesheet" href="../../static/stylesheets/admin-css/960.css" type="text/css" media="screen" charset="utf-8" />\r\n                <link rel="stylesheet" href="../../static/stylesheets/admin-css/template.css" type="text/css" media="screen" charset="utf-8" />\r\n                <link rel="stylesheet" href="../../static/stylesheets/admin-css/colour.css" type="text/css" media="screen" charset="utf-8" />\r\n                <!--[if IE]><![if gte IE 6]><![endif]-->\r\n             \u3000\u3000<link rel="icon" href="../../static/image/favicon.ico" mce_href="../../static/image/favicon.ico" type="image/x-icon">\r\n                <script src="../../static/javascripts/admin-js/glow/1.7.0/core/core.js" type="text/javascript"></script>\r\n                <script src="../../static/javascripts/jquery-1.7.2.min.js" type="text/javascript"></script>\r\n                <script src="../../static/javascripts/admin-js/glow/1.7.0/widgets/widgets.js" type="text/javascript"></script>\r\n                <script src="../../static/javascripts/displayMessage.js" type="text/javascript"></script>\r\n                <link href="../../static/javascripts/admin-s/glow/1.7.0/widgets/widgets.css" type="text/css" rel="stylesheet" />\r\n                ')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'extra'):
             context['self'].extra(**pageargs)
         
 
-        # SOURCE LINE 16
+        # SOURCE LINE 17
         __M_writer('\r\n                <script type="text/javascript">\r\n                        glow.ready(function(){\r\n                                new glow.widgets.Sortable(\r\n                                        \'#content .grid_5, #content .grid_6\',\r\n                                        {\r\n                                                draggableOptions : {\r\n                                                        handle : \'h2\'\r\n                                                }\r\n                                        }\r\n                                );\r\n                        });\r\n                </script>\r\n                <!--[if IE]><![endif]><![endif]-->\r\n        </head>\r\n        <body>\r\n\r\n          <h1 id="head">PPTM News 管理后台</h1>\r\n          ')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'navigation'):
             context['self'].navigation(**pageargs)
         
 
-        # SOURCE LINE 34
-        __M_writer('\r\n            ')
         # SOURCE LINE 35
+        __M_writer('\r\n            ')
+        # SOURCE LINE 36
         __M_writer(str(self.body()))
         __M_writer('\r\n        </body>\r\n</html>\r\n')
         return ''
